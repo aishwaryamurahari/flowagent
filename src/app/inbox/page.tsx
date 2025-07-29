ea0034193ce4093f28aadc2d7b0a661cf17d913d
Aishwarya Murahari <aishwarya26ramagiri@gmail.com>
2025-07-29 16:13:02 -0700

    fix aws amplify build errors

--- src/app/inbox/page.tsx
+++ src/app/inbox/page.tsx
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth/next";
-import { authOptions } from "../api/auth/[...nextauth]/route";
+import { authOptions } from "@/lib/auth";
 import { fetchEmails } from "@/lib/gmail";
 import Link from "next/link";
 
